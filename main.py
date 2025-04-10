68e75c95ae8fb4091276af2cb75ca7b753ba525d
Krishna Lodhi <45414105+Krishnalodhi@users.noreply.github.com>
2025-04-11 02:48:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+yes="y"
+while yes=="y":
+    def game(num):
+        with open("heigh.txt") as heigh:
+            score=heigh.read()
+            if score=="":
+                with open("heigh.txt","w") as new:
+                    score=new.write(str(num))
+                    print("\nnew heigh score is :- ",num)
+            elif int(score) > num:
+                print("\nbetter luck next time !",num)
+            else:
+                print("\nyou win :) \n","your new score :- "num,"\n")
+                with open("heigh.txt","w") as new:
+                    new.write(str(num))
+        yes=input("\nwant to play again y/n :- ")
+    num = random.randint(1,99)
+    game(num)
